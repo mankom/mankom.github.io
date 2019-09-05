bae9a07e6c2d96bbb0652b467a35b74c1b50ad6e
Marek Mańko <merak.manko@gmail.com>
2019-09-05 15:12:40 +0200

    feat: my projects section js script and css

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,32 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
-console.log(`Hello world!`);
+console.log(`Hey there!`);
+
+const list = document.querySelector('.my-projects__list--js');
+
+fetch('https://api.github.com/users/mankom/repos?sort=updated&direction=desc')
+.then(reps => reps.json())
+.then(reps => {
+  const repos = reps;
+  for(const repo of repos){
+    const {homepage, description, html_url, name} = repo;
+    if(homepage){
+      list.innerHTML += `
+      <li class="my-projects__item">
+        <a class="my-projects__link" href="${html_url}">${name}</a>
+        <p class="my-projects__description">${description ? description : "brak opisu"}</p>
+      </li>
+    `;
+    }else{
+      break;
+    }
+    
+    console.log(repo);
+  }
+})
+.catch(err => {
+
+})
 
 
