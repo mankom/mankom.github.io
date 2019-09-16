1ae27a831d7714d204c4d60ae4544cfae1330467
Marek Mańko <merak.manko@gmail.com>
2019-09-16 12:55:06 +0200

    feat: fix getting repositories from github

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,12 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
-
-console.log(`Hey there!`);
-
-const list = document.querySelector('.my-projects__list--js');
+const list=document.querySelector(".my-projects__list--js");
 
 fetch('https://api.github.com/users/mankom/repos?sort=updated&direction=desc')
 .then(reps => reps.json())
 .then(reps => {
   const repos = reps;
+  console.log(repos);
   for(const repo of repos){
     const {homepage, description, html_url, name} = repo;
     if(homepage){
@@ -35,15 +17,11 @@ fetch('https://api.github.com/users/mankom/repos?sort=updated&direction=desc')
         <p class="my-projects__description">${description ? description : "brak opisu"}</p>
       </li>
     `;
-    }else{
-      break;
     }
-    
-    console.log(repo);
   }
 })
 .catch(err => {
-
+  console.log(err);
 })
 
 
